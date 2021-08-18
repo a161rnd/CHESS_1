283e98648dbef3ef10de2c598b83f63f4f206c6f
Администратор <a161rnd@gmail.com>
2021-08-18 16:33:39 +0300

    added Pawn

---
+++ src/Pawn.java
@@ -0,0 +1,40 @@
+public class Pawn extends ChessPiece {
+
+    public Pawn(String color) {
+        super(color);
+    }
+
+    @Override
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        final int STARTWHITE = 1;
+        final int STARTBLACK = 6;
+        if ((checkMove(toLine) && checkMove(toColumn) && checkMove(line) && checkMove(column)) &&
+                (toLine != line && toColumn == column)) {
+            if (((color == "White" && line > STARTWHITE && (toLine - line) == 1)) ||
+                    ((color == "Black" && line < STARTBLACK && (line - toLine) == 1))) return true;
+        } else return false;
+        if ((checkMove(toLine) && checkMove(toColumn) && checkMove(line) && checkMove(column)) &&
+                (toLine != line && toColumn == column)) {
+            if (((color == "White" && line == STARTWHITE) && (((toLine - line) == 2) || ((toLine - line) == 1))) ||
+            (((color == "Black" && line == STARTBLACK)) && (((line - toLine) == 2) || ((line - toLine) == 1))))
+                return true;
+            else return false;
+        } else return false;
+    }
+
+
+    @Override
+    public String getSymbol() {
+        return "P";
+    }
+
+    boolean checkMove(int pos) {
+        return pos >= 0 && pos <= 7;
+    }
+}
+
--- src/TestMe1.java
+++ src/TestMe1.java
@@ -2,5 +2,11 @@ public class TestMe1 {
     public static void main(String[] args) {
         Horse horseW1 = new Horse("white");
         System.out.println(horseW1.canMoveToPosition(new ChessBoard("white"), 0, 7, 1, 5));
+        System.out.println();
+
+        Pawn pawnW = new Pawn ("White");
+        Pawn pawnB = new Pawn ("Black");
+        System.out.println(pawnW.canMoveToPosition(new ChessBoard("White"), 2, 3, 3, 3));
+        System.out.println(pawnB.canMoveToPosition(new ChessBoard("Black"), 5, 4, 4, 5));
     }
 }
