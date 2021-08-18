d3712a9233eeca27e527ace5740b5f5d314c09df
Администратор <a161rnd@gmail.com>
2021-08-18 17:12:30 +0300

    added Rook

--- src/TestMe1.java
+++ src/TestMe1.java
@@ -1,5 +1,7 @@
 public class TestMe1 {
     public static void main(String[] args) {
+        ChessBoard chessBoard1 = new ChessBoard("White");
+
         Horse horseW1 = new Horse("white");
         System.out.println(horseW1.canMoveToPosition(new ChessBoard("white"), 0, 7, 1, 5));
         System.out.println();
@@ -12,6 +14,12 @@ public class TestMe1 {
 
         Bishop bishop = new Bishop("White");
         System.out.println(bishop.canMoveToPosition(new ChessBoard("White"), 7,5,4,2));
+
+        System.out.println("\nЛадья:");
+
+        Rook rook = new Rook("Black");
+        System.out.println(rook.canMoveToPosition(chessBoard1, 6,4,6,7));
+
 
 
     }
